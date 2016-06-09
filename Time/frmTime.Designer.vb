482ee0e547387a23947294a4930b98df16d7e373
Michael-Nunley <Michael.ray.nunley@gmail.com>
2016-06-09 12:26:23 -0700

    init

---
+++ Time/frmTime.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTime
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.cmdExit = New System.Windows.Forms.Button()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.tmrUpdate = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'cmdExit
+        '
+        Me.cmdExit.Location = New System.Drawing.Point(18, 36)
+        Me.cmdExit.Name = "cmdExit"
+        Me.cmdExit.Size = New System.Drawing.Size(308, 23)
+        Me.cmdExit.TabIndex = 1
+        Me.cmdExit.Text = "Exit"
+        Me.cmdExit.UseVisualStyleBackColor = True
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTime.Location = New System.Drawing.Point(12, 0)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(314, 33)
+        Me.lblTime.TabIndex = 2
+        Me.lblTime.Text = "0/00/0000 00:00:00 AM"
+        '
+        'tmrUpdate
+        '
+        Me.tmrUpdate.Enabled = True
+        Me.tmrUpdate.Interval = 1000
+        '
+        'frmTime
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(339, 62)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.cmdExit)
+        Me.Name = "frmTime"
+        Me.Opacity = 0.5R
+        Me.Text = "The time!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdExit As System.Windows.Forms.Button
+    Friend WithEvents lblTime As System.Windows.Forms.Label
+    Friend WithEvents tmrUpdate As System.Windows.Forms.Timer
+
+End Class
---
+++ Time/frmTime.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmTime
+    Private Sub cmdExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdExit.Click
+        Me.Close()
+    End Sub
+    Private Sub tmrUpdate_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrUpdate.Tick
+        lblTime.Text = System.DateTime.Now
+    End Sub
+End Class
